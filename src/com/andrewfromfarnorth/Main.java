787f2ef80ea20f66409cb7710d4a9e42abb7ace1
Andrew <andrew_h_siberian@rambler.ru>
2018-09-18 23:56:34 +0700

    Update Main.java

--- src/com/andrewfromfarnorth/Main.java
+++ src/com/andrewfromfarnorth/Main.java
@@ -1,14 +1,8 @@
 package com.andrewfromfarnorth;
 
-//import java.util.logging.*;
-//import java.util.regex.*;
-
-
 public class Main {
 
     public static void main(String[] args) {
-        /*int x = 2;
-        System.out.println(5 / x);*/
         int[] array = {1, 3, 5, 7, 9};
         VagueList vagueList = new VagueList(array);
         System.out.println(vagueList.getLength());
